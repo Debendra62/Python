9a7eef46b014024acf7870741ab96bcb425d0fd1
Debendra Bakhati <debendrabakhati762@gmail.com>
2022-07-31 19:45:43 +0545

    string more used

---
+++ basic program/string more syntax.py
@@ -0,0 +1,35 @@
+a="good morning"
+b="Good Evening"
+c="GOOD NIGHT"
+
+print(a.capitalize())   #changes first charter to capital and all others to small 
+print(b.capitalize())
+print(c.capitalize())
+print(end='\n')
+
+print(a.casefold())     #Change all character to small
+print(b.casefold())
+print(c.casefold())
+print(end='\n')
+
+print(a.center(20,'@'))      #Moves 20 space towards the centre
+print(b.center(20,'@'))
+print(c.center(20,'@'))
+print(end='\n')
+
+print(a.count('o'))       #Number of time specified value occures in string
+print(b.count('o'))
+print(c.count('o'))
+print(end='\n')
+
+print(a.swapcase())        #change upper case to lower case and vice versa    
+print(b.swapcase())  
+print(c.swapcase())
+print(end='\n')
+
+print(b.translate())  
+
+
+
+
+
