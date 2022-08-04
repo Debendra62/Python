6520f62fa09ad18200867c50e0b8bd56124856fe
Debendra Bakhati <debendrabakhati762@gmail.com>
2022-08-04 10:31:26 +0545

    comprehension ,import added

--- basic program/pratice.py
+++ basic program/pratice.py
@@ -7,3 +7,6 @@ domain=email[email.index("@") + 1:email.index(".")]
 print("your name is {} and domain is {} .".format(name2,domain))
 print("your domain is {1} and name is {0} .".format(name2,domain))
 print(f"your name is {name2} and domain is {domain} .")
+
+import mymodule
+mymodule.greeting("dev")
